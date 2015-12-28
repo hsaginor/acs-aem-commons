5d245ef16c17f68fb87b7730ec5a3d0c5b9c37cd
Henry Saginor <hsaginor@gmail.com>
2015-12-28 11:06:24 -0800

    Added some debug level logging and comments.

--- bundle/src/main/java/com/adobe/acs/commons/oak/impl/EnsureOakIndexListener.java
+++ bundle/src/main/java/com/adobe/acs/commons/oak/impl/EnsureOakIndexListener.java
@@ -1,6 +1,8 @@
 package com.adobe.acs.commons.oak.impl;
 
 import javax.annotation.Nonnull;
+import javax.jcr.RepositoryException;
+import javax.jcr.observation.Event;
 import javax.jcr.observation.EventIterator;
 import javax.jcr.observation.EventListener;
 
@@ -9,6 +11,11 @@ import org.apache.sling.commons.scheduler.Scheduler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * This event listener will run EnsureOakIndexJobHandler as sling job when Ensure Definitions are updated.
+ * @author hsaginor
+ *
+ */
 public class EnsureOakIndexListener implements EventListener {
 
 	private static final Logger log = LoggerFactory.getLogger(EnsureOakIndexListener.class);
@@ -25,10 +32,14 @@ public class EnsureOakIndexListener implements EventListener {
 	
 	@Override
 	public void onEvent(EventIterator events) {
+		logEvents(events);
 		executeJob();
 	}
 
 	void executeJob() {
+		// TODO: Currently EnsureOakIndexJobHandler is implemented to process entire Ensure definitions tree.
+		// We should be able to optimize this by implementing a job that will process specific changes only. 
+		
 		Scheduler scheduler = ensureIndex.getScheduler();
 		EnsureOakIndexJobHandler jobHandler =
                 new EnsureOakIndexJobHandler(ensureIndex, oakIndexPath, ensureDefinitionsPath);
@@ -39,6 +50,36 @@ public class EnsureOakIndexListener implements EventListener {
         scheduler.schedule(jobHandler, options);
 
         log.info("Job scheduled for ensuring Oak Indexes [ {} ~> {} ]", ensureDefinitionsPath, oakIndexPath);
+	}
+	
+	private void logEvents(EventIterator events) {
+		if(log.isDebugEnabled()) {
+			while(events.hasNext()) {
+				Event e = events.nextEvent();
+				try {
+					log.debug("Recieved JCR event " + getEventTypeFromEvent(e) + " for " + e.getPath());
+				} catch (RepositoryException e1) {}
+			}
+		}
+	}
+	
+	private String getEventTypeFromEvent(Event e) {
+		String type = "";
+		switch(e.getType()) {
+			case Event.NODE_ADDED : type = "NODE_ADDED";
+				break;
+			case Event.NODE_MOVED : type = "NODE_MOVED";
+				break;
+			case Event.NODE_REMOVED : type = "NODE_REMOVED";
+				break;
+			case Event.PROPERTY_ADDED : type = "PROPERTY_ADDED";
+				break;
+			case Event.PROPERTY_CHANGED : type = "PROPERTY_CHANGED";
+				break;
+			case Event.PROPERTY_REMOVED : type = "PROPERTY_REMOVED";
+				break;
+		}
+		return type;
 	}
 
 }
